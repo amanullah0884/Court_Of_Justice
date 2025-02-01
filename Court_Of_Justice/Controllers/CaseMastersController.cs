a9d651561e42723197df0c84414b7461127bdc4b
AMAN ULLAH <amanullah0884@gmail.com>
2025-02-01 17:04:18 +0600

    CaseMaster

---
+++ Court_Of_Justice/Controllers/CaseMastersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Court_Of_Justice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CaseMastersController : ControllerBase
+    {
+    }
+}
