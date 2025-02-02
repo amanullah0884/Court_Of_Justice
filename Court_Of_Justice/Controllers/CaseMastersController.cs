a58202a03d32e5f633441b216b93aced2412d0ac
amanullah0884 <amanullah0884@gmail.com>
2025-02-03 00:02:46 +0600

    CaseMaster And SOurces

--- Court_Of_Justice/Controllers/CaseMastersController.cs
+++ Court_Of_Justice/Controllers/CaseMastersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Court_Of_Justice.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Court_Of_Justice.Controllers
@@ -7,5 +8,70 @@ namespace Court_Of_Justice.Controllers
     [ApiController]
     public class CaseMastersController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        public CaseMastersController(ApplicationDbContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet]
+        public IEnumerable<CaseMaster> Get()
+        {
+            return _context.CaseMasters.ToList();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<CaseMaster>GetAll(int id)
+        {
+            var CaseM = _context.CaseMasters.Find(id);
+            if(CaseM == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(CaseM);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Post(CaseMaster caseMaster)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            _context.CaseMasters.Add(caseMaster);
+            _context.SaveChanges();
+            return CreatedAtRoute(new { id = caseMaster.ID }, caseMaster);
+
+        }
+
+        [HttpPut]
+        public IActionResult Put(int id, CaseMaster caseMaster)
+        {
+            var existingCaseM = _context.CaseMasters.Find(id);
+            if (existingCaseM == null)
+            {
+                return NotFound();
+            }
+            existingCaseM.ID = caseMaster.ID;
+            existingCaseM.Description = caseMaster.Description;
+            _context.SaveChanges();
+            return Ok(existingCaseM);
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var caseM = _context.CaseMasters.Find(id);
+            if (caseM == null)
+            {
+                return NotFound();
+            }
+
+            _context.CaseMasters.Remove(caseM);
+            _context.SaveChanges();
+            return Ok();
+        }
+
     }
 }
---
+++ Court_Of_Justice/Controllers/CaseSourcesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Court_Of_Justice.Models;
+
+namespace Court_Of_Justice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CaseSourcesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CaseSourcesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CaseSources
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CaseSource>>> GetCaseSources()
+        {
+            return await _context.CaseSources.ToListAsync();
+        }
+
+        // GET: api/CaseSources/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CaseSource>> GetCaseSource(int id)
+        {
+            var caseSource = await _context.CaseSources.FindAsync(id);
+
+            if (caseSource == null)
+            {
+                return NotFound();
+            }
+
+            return caseSource;
+        }
+
+        // PUT: api/CaseSources/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCaseSource(int id, CaseSource caseSource)
+        {
+            if (id != caseSource.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(caseSource).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CaseSourceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CaseSources
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CaseSource>> PostCaseSource(CaseSource caseSource)
+        {
+            _context.CaseSources.Add(caseSource);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCaseSource", new { id = caseSource.ID }, caseSource);
+        }
+
+        // DELETE: api/CaseSources/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCaseSource(int id)
+        {
+            var caseSource = await _context.CaseSources.FindAsync(id);
+            if (caseSource == null)
+            {
+                return NotFound();
+            }
+
+            _context.CaseSources.Remove(caseSource);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CaseSourceExists(int id)
+        {
+            return _context.CaseSources.Any(e => e.ID == id);
+        }
+    }
+}
